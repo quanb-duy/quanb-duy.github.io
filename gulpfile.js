ae1261384d82ef259bfa0cdafd18280c9659d0c9
quanb-duy <quanbuid@gmail.com>
2024-02-06 16:27:08 +0100

    start

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const { src, dest, watch, series, parallel } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const terser = require("gulp-terser");
+const browsersync = require("browser-sync").create();
+
+// sass task
+function scssTask() {
+  return src("app/scss/style.scss", { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()])) // Added parentheses to invoke cssnano
+    .pipe(dest("dist", { sourcemaps: "." }));
+}
+
+// Javascript Task
+function jsTask() {
+  return src("app/js/script.js", { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest("dist", { sourcemaps: "." }));
+}
+
+// Browsersync Tasks
+function browsersyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+  cb();
+}
+
+function browserSyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  watch("*.html", browserSyncReload);
+  watch(
+    ["app/scss/**/*.scss", "app/js/**/*.js"],
+    series(scssTask, jsTask, browserSyncReload)
+  );
+}
+
+// Default Gulp task
+exports.default = series(
+  parallel(scssTask, jsTask, browsersyncServe),
+  watchTask
+);
